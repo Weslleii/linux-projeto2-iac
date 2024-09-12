a1bf13fa6fb9c151787d9a162db9da94ff204e3c
Wesleiii <wesleigomes19@gmail.com>
2024-09-11 21:39:35 -0300

    Apache v.1.0

---
+++ script-iac2.sh
@@ -0,0 +1,18 @@
+#!/bim/bash
+
+echo "Atualizando o servidor...
+"
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+
+echo "Baixando e copiando os arquivos da aplicação..."
+
+cd /tmp
+wget  https://github.com/Weslleii/TeamDevOps/archive/refs/heads/main.zip
+unzip main.zip
+cd TeamDevOps
+cp -R * /var/www/html/
+
